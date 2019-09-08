10500e34b7bea4233963ca17c639d5c6fa48d217
minkyungjun <q_alz@naver.com>
2019-09-08 14:10:44 +0900

    Initial Commit

---
+++ serachAPITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  serachAPITest
+//
+//  Created by 민경준 on 08/09/2019.
+//  Copyright © 2019 민경준. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
